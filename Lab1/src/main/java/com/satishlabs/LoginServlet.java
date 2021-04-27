18fb9544899a495e2cd84f5c315ffc8a087d6b52
Satish <spd61185@gmail.com>
2021-04-28 00:08:06 +0530

    Lab1

---
+++ Lab1/src/main/java/com/satishlabs/LoginServlet.java
@@ -0,0 +1,40 @@
+package com.satishlabs;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+//@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+ 
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//1. Collect the data
+		String uname = request.getParameter("uname");
+		String pword = request.getParameter("pword");
+		String msg="";
+		
+		//2. Verifying or Processing the data 
+		if(uname.equals(pword)) {
+			msg = "<h1>Login Success <br /> Welcome to Satish Labs</h1>";
+		}else {
+			msg = "<h1>Login Success <br /> Invalid Username or Password</h1>";
+		}
+		
+		//3. Prepare the response
+		Writer out = response.getWriter();
+		out.write(msg);
+	}
+
+}
