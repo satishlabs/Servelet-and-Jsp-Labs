eb4df8b89c29a37fafbee793042b2a3ee479802f
Satish <spd61185@gmail.com>
2021-05-05 20:45:16 +0530

    Lab8

---
+++ Lab8/src/main/java/com/satishlabs/TestServlet.java
@@ -0,0 +1,33 @@
+package com.satishlabs;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("********* TestServlet  service() **************");
+		response.setContentType("text/html");
+
+		Writer out = response.getWriter();
+		out.write("<h1>Welcome to Satish Labs</h1>");
+
+
+	}
+
+}
