c7100f8ecb60c312c6077072a6c312872421b373
Satish Prasad <spd61185@gmail.com>
2021-05-31 07:11:23 +0530

    Lab19

---
+++ Lab19/src/main/java/com/satishlabs/TestServlet.java
@@ -0,0 +1,29 @@
+package com.satishlabs;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		Writer out = response.getWriter();
+		out.write("<h1>Id: </h1>"+session.getId());
+		out.write("<h1>isNew: </h1>"+session.isNew());
+	}
+
+}
